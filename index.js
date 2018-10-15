ec647dcd241a8215ae2e02eabaa9517f4eb83967
Piyush Maurya <piyush.maurya@live.in>
2018-10-15 23:31:43 +0530

    Few more routes added

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.send("Hello World!");
+  res.send({ message: "Hello World!" });
+});
+
+app.get("/api", function(req, res) {
+  res.send({ message: "Hello World!" });
+});
+
+app.get("*", function(req, res) {
+  res.send({ error: "Undefined Route" });
 });
 
 module.exports.handler = serverless(app);
